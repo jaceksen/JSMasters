66fe244819e8903ccc9d59e7a821e64ed57faddf
jaceksen <jaceksen@gmail.com>
2018-12-18 13:31:50 +0100

    Try catch

---
+++ lekcje/lekcja12/js1.js
@@ -0,0 +1,19 @@
+
+
+
+console.log('Tralalala');
+
+try {
+    function1();
+} catch(e) {
+    console.log('Wyjebunda');
+    console.log(e);
+} finally {
+    console.log('To zadzaiła zawsze');
+}
+
+// function function1() {
+//     console.log('Jestem w funkcji1');
+// }
+
+console.log('Tralalal2');
